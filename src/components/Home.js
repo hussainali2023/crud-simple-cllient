e25fd8eb381fe832b2aec30e484a1859fb1073b3
hussain2023 <iamhussainali2@gmail.com>
2022-11-06 20:24:24 +0530

    deleted user from database

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const users = useLoaderData();
-  console.log(users);
+  const [displayUsers, setDisplayUsers] = useState(users);
+
+  const handleDelete = (user) => {
+    const agree = window.confirm(
+      `Are you wand to sure to delete: ${user.name}`
+    );
+    if (agree) {
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            const remainingUsers = displayUsers.filter(
+              (usr) => usr._id !== user._id
+            );
+            setDisplayUsers(remainingUsers);
+          }
+        });
+    }
+  };
   return (
     <div>
-      <h1>Total Users: {users.length}</h1>
+      <h1>Total Users: {displayUsers.length}</h1>
       <div>
-        {users.map((user) => (
-          <li key={user._id}>{user.name}</li>
+        {displayUsers.map((user) => (
+          <div>
+            <li key={user._id}>
+              {user.name} <button onClick={() => handleDelete(user)}>X</button>
+            </li>
+          </div>
         ))}
       </div>
     </div>
